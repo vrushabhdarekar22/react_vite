f1ece16fc7474c417f343c99356c55e87acd94bd
vrushabhdarekar <vrushabhdarekar45@gmail.com>
2025-02-06 13:14:38 +0530

    setup tailwindcss

---
+++ myapp/src/components/HeaderName.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HeaderName = () => {
+  return (
+    <div className='h-screen w-auto bg-gray-800 flex justify-center items-center'>
+      <h1 className="text-green-600 text-5xl font-extrabold">Vrushabh Darekar</h1>
+    </div>
+  )
+}
+
+export default HeaderName
+
